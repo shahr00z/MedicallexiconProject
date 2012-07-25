5665556a858ce7a6493404808c4f705aaafb43de
shahrooz <sh.mjsoft@gmail.com>
2012-07-25 20:06:17 +0430

    Create GetEntityCount Action and render partial  in index

--- MedicallexiconProject/Controllers/HomeController.cs
+++ MedicallexiconProject/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
 ﻿using System.Web.Mvc;
+using DataLayer.Context;
+using MedicallexiconProject.ViewModel;
+using ServiceLayer.Interfaces;
 
 namespace MedicallexiconProject.Controllers
 {
 	public partial class HomeController : Controller
 	{
+		private readonly ILanguageService _languageService;
+		private readonly IUnitOfWork _uow;
+		private readonly IWordService _wordService;
+
+		public HomeController(IUnitOfWork uow, ILanguageService languageService, IWordService wordService)
+		{
+			_uow = uow;
+			_languageService = languageService;
+			_wordService = wordService;
+		}
+
 		public virtual ActionResult Index()
 		{
-			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
 			return View();
 		}
 
@@ -19,6 +31,18 @@ namespace MedicallexiconProject.Controllers
 		public virtual ActionResult Contact()
 		{
 			return View();
+		}
+
+		public virtual ActionResult GetEntityCount()
+		{
+			var countViewModel = new CountViewModel();
+
+			int languageCount = _languageService.GetCount();
+			int wordCount = _wordService.GetCount();
+			countViewModel.LanguageCount = languageCount;
+			countViewModel.WordCount = wordCount;
+
+			return PartialView(MVC.Home.Views._GetEntityCount,countViewModel);
 		}
 	}
 }
